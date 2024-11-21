e93d4103b03f50bdf0ad66bcf919e4d0b0ee9b54
SyPhase <pi2568gamerz@gmail.com>
2024-11-20 23:12:25 -0700

    Coded rotation for Turret
    - Coded rotation for Turret
    - Created turret model out of boxes
    - Added a terrain instead of plane

---
+++ Assets/Tower/Control.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    // Can be set in the Inspector
+    [SerializeField] float xAxisSensitivity = 10f;
+    [SerializeField] float yAxisSensitivity = 10f;
+
+    [SerializeField] float xAxisMaxSpeed = 1f;
+
+    // Private variables
+    Rigidbody rigidbody;
+
+    float xAxis = 0f;
+    float yAxis = 0f;
+    float fire = 0f;
+
+    // Start is called once when the scene finishes loading
+    private void Start()
+    {
+        // Get reference to rigidbody component on this object
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Set maximum angular velocity (clamp) so it won't spin too fast
+        rigidbody.maxAngularVelocity = xAxisMaxSpeed;
+    }
+
+    // FixedUpdate is called once per physics time set (0.02s)
+    void FixedUpdate()
+    {
+        // Get and store Inputs
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        fire = Input.GetAxis("Jump");
+
+        // If no xAxis input
+        if (xAxis == 0f)
+        {
+            // Set angular velocity to zero if very low
+            if (0.05f > rigidbody.angularVelocity.y && -0.05f < rigidbody.angularVelocity.y)
+            {
+                rigidbody.angularVelocity = Vector3.zero;
+            }
+            // if rotating right, set input to counter-rotate left
+            else if (0 < rigidbody.angularVelocity.y)
+            {
+                xAxis = -1f;
+            }
+            // if rotating left, set input to counter-rotate right
+            else
+            {
+                xAxis = 1f;
+            }
+        }
+
+        // Rotate turret arount y-axis by adding torque (ForceMode isn't important)
+        rigidbody.AddTorque(0f, xAxis * xAxisSensitivity, 0f, ForceMode.Acceleration);
+
+        // Debug
+        //print("y Angular vel = " + rigidbody.angularVelocity.y);
+        //print("xAxis = " + xAxis);
+    }
+}
